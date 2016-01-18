7b609e29ac6de67ff1d69bab17eb5d85de640608
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2016-01-18 01:46:40 +0100

    useridaLssaads

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -25,6 +25,7 @@ exports.create = function(req,res){
 
 	userController.autenticar(login, password, function(error, user) {
 
+consoel.log(error);
 		if(error) { //si hay error retornamos mensajes de error de sesion
 			req.session.errors = [{"message": 'Se ha producido un error:' + error}];
 			res.redirect("/login");
